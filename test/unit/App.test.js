675191a308484c6cb94d05f6d5ffa1ed4f0c48a3
tcurteanu <mihai.curteanu@williamhill.com>
2018-07-31 11:32:14 +0200

    new test

--- test/unit/App.test.js
+++ test/unit/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GamesList from '../../src/containers/GamesList';
+import Game from '../../src/containers/Game';
 
-it('renders without crashing', () => {
+it('renders GamesList without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<GamesList />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders Game without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Game />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
